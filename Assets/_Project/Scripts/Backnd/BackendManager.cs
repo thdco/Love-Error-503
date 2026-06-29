fe2b13a11ae6a1d8b514f89cf40226a50dfa89d2
thdco <sarahsong10@naver.com>
2026-06-29 22:50:40 +0900

    test: Backend 로그인 및 게임 정보 기능 테스트

---
+++ Assets/_Project/Scripts/Backnd/BackendManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+
+// 뒤끝 SDK namespace 추가
+using BackEnd;
+
+public class BackendManager : MonoBehaviour
+{
+    void Start()
+    {
+        var bro = Backend.Initialize(); // 뒤끝 초기화
+
+        // 뒤끝 초기화에 대한 응답값
+        if (bro.IsSuccess())
+        {
+            Debug.Log("초기화 성공 : " + bro); // 성공일 경우 statusCode 204 Success
+        }
+        else
+        {
+            Debug.LogError("초기화 실패 : " + bro); // 실패일 경우 statusCode 400대 에러 발생 
+        }
+
+        Test();
+    }
+
+    // 동기 함수를 비동기에서 호출하게 해주는 함수(유니티 UI 접근 불가)
+    void Test()
+    {
+        BackendLogin.Instance.CustomLogin("user1", "1234"); // 뒤끝 로그인
+
+        BackendGameData.Instance.GameDataGet(); // 데이터 삽입 함수
+
+        // [추가] 서버에 불러온 데이터가 존재하지 않을 경우, 데이터를 새로 생성하여 삽입
+        if (BackendGameData.userData == null)
+        {
+            BackendGameData.Instance.GameDataInsert();
+        }
+
+        BackendGameData.Instance.LevelUp(); // [추가] 로컬에 저장된 데이터를 변경
+
+        BackendGameData.Instance.GameDataUpdate(); //[추가] 서버에 저장된 데이터를 덮어씌기(변경된 부분만)
+
+        Debug.Log("테스트를 종료합니다.");
+    }
+}
